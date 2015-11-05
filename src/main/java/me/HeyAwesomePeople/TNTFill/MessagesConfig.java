de2566e00b19e2f0dd4ae119a822c226f07e4dbe
Bryan Tift <bryantift@rocketmail.com>
2015-11-05 14:50:47 -0800

    Updates

---
+++ src/main/java/me/HeyAwesomePeople/TNTFill/MessagesConfig.java
@@ -0,0 +1,50 @@
+package me.HeyAwesomePeople.TNTFill;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+public class MessagesConfig {
+
+    private TNTFill plugin = TNTFill.instance;
+
+    private FileConfiguration customConfig = null;
+    private File customConfigFile = null;
+
+    public void reloadCustomConfig() {
+        if (customConfigFile == null) {
+            customConfigFile = new File(plugin.getDataFolder(), "messages.yml");
+        }
+        customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+
+        // Look for defaults in the jar
+        InputStream defConfigStream = plugin.getResource("messages.yml");
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            customConfig.setDefaults(defConfig);
+        }
+    }
+
+    public FileConfiguration getCustomConfig() {
+        if (customConfig == null) {
+            reloadCustomConfig();
+        }
+        return customConfig;
+    }
+
+    public void saveCustomConfig() {
+        if (customConfig == null || customConfigFile == null) {
+            return;
+        }
+        try {
+            getCustomConfig().save(customConfigFile);
+        } catch (IOException ex) {
+           plugin.getLogger().log(Level.SEVERE, "Could not save config to " + customConfigFile, ex);
+        }
+    }
+
+}
--- src/main/java/me/HeyAwesomePeople/TNTFill/TNTFill.java
+++ src/main/java/me/HeyAwesomePeople/TNTFill/TNTFill.java
@@ -1,5 +1,6 @@
 package me.HeyAwesomePeople.TNTFill;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -12,13 +13,22 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.*;
 
 public class TNTFill extends JavaPlugin implements Listener {
+    public static TNTFill instance;
     private File fileconfig = new File(this.getDataFolder() + File.separator + "config.yml");
+
+    public List<UUID> autoFill = new ArrayList<UUID>();
+
+    public MessagesConfig messagesConfig;
 
     @Override
     public void onEnable() {
+        instance = this;
         getServer().getPluginManager().registerEvents(new TNTListener(this), this);
+
+        messagesConfig = new MessagesConfig();
 
         if (!fileconfig.exists()) {
             this.saveDefaultConfig();
@@ -30,12 +40,71 @@ public class TNTFill extends JavaPlugin implements Listener {
         reloadConfig();
     }
 
+    public String getMessage(String s) {
+        return ChatColor.translateAlternateColorCodes('&', messagesConfig.getCustomConfig().getString(s));
+    }
+
+    public String prefix() {
+        return getConfig().getString("prefix");
+    }
+
     public boolean onCommand(final CommandSender sender, Command cmd,
                              String commandLabel, final String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can run this command!");
+            return false;
+        }
+        Player p = (Player) sender;
+        if (args.length == 0) {
+            p.sendMessage(ChatColor.BLUE + "=--= TNTFill =--=");
+            p.sendMessage(ChatColor.GOLD + "/tntfill (amount) - Fill a dispenser with TNT");
+            p.sendMessage(ChatColor.GOLD + "/tntfull auto - Toggle auto tnt fill");
+            p.sendMessage(ChatColor.GOLD + "/tntclear (radius) - Clear all tnt within a radius");
+        }
         if (commandLabel.equalsIgnoreCase("tntfill")) {
-
+            if (args[0].equalsIgnoreCase("auto")) {
+                if (!p.hasPermission("tntfill.auto")) {
+                    p.sendMessage(prefix() + ChatColor.RED + "You do not have permissions to use this command!");
+                    return false;
+                }
+                if (autoFill.contains(p.getUniqueId())) {
+                    autoFill.remove(p.getUniqueId());
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix()));
+                } else {
+                    autoFill.add(p.getUniqueId());
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix()));
+                }
+                return false;
+            } else if (isInteger(args[0])) {
+                if (!p.hasPermission("tntfill.perform")) {
+                    p.sendMessage(prefix() + ChatColor.RED + "You do not have permissions to use this command!");
+                    return false;
+                }
+                if (p.getTargetBlock((Set<Material>) null, 7) == null) {
+                    // TODO message
+                    return false;
+                }
+                Block b = p.getTargetBlock((Set<Material>) null, 7);
+                if (!b.getType().equals(Material.DISPENSER)) {
+                    // TODO message
+                    return false;
+                }
+                Dispenser dis = (Dispenser) b.getState();
+                for (int i = 0; i <= 128; i++) { //TODO change amount
+                    dis.getInventory().addItem(new ItemStack(Material.TNT));
+                }
+                p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix() + "".replace("%amount%", 128 + /* TODO */ "")));
+                // TODO fill dispenser
+            } else {
+                p.sendMessage(prefix() + ChatColor.RED + "Invalid subcommand!");
+            }
         }
         if (commandLabel.equalsIgnoreCase("tntclear")) {
+            if (args.length == 0) {
+
+            } else {
+
+            }
         }
         return false;
     }
@@ -52,15 +121,22 @@ public class TNTFill extends JavaPlugin implements Listener {
                     if (block.getType().equals(Material.DISPENSER)) {
                         if (block.getState() == null) continue;
                         Dispenser dis = (Dispenser) block.getState();
-                        for (int i = 0; i <= dis.getInventory().getSize(); i++) {
-                            dis.getInventory().addItem(new ItemStack(Material.TNT, 64));
-                        }
+                        dis.getInventory().clear();
                     }
                 }
             }
         }
     }
 
+    public boolean isInteger(String s) {
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 }
 
